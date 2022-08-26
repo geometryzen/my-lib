9515667bba54eca9ee67560284b6c0fd01ed481f
David Geo Holmes <geometryzen@gmail.com>
2022-08-26 16:27:43 -0400

    Initial

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+// import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+
+const packageJson = require('./package.json');
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.browser,
+                format: 'umd',
+                sourcemap: true,
+                name: "stemcstudioMechanicsJsxGraph",
+            },
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                sourcemap: true,
+                name: 'stemcstudioMechanicsJsxGraph'
+            },
+            {
+                file: packageJson.module,
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: packageJson.system,
+                format: 'system',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            external(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            // terser()
+        ]
+    },
+    {
+        input: 'dist/esm/index.d.ts',
+        output: [{ file: packageJson.types, format: "esm" }],
+        plugins: [dts()],
+    }
+]
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export function hello(name: string): void {
+    console.log(`Hello, ${name}`);
+}
+
+/**
+ * Constructs a personalized string that can be used to greet a person.
+ * @param name The name of the person receiving the greeting.
+ * @returns a greeting string containing the name of the person receiving the greeting.
+ */
+export function greeting(name: string): string {
+    return `Hello, ${name}!`;
+}
